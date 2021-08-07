6f32e5fd270a562ebafd04b6b66ed78fd186be0c
Sandeep <sandeep.prajapati@contriverz.com>
2021-08-07 14:17:28 +0530

    firstcommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+  import { NgModule } from '@angular/core';
+  import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-];
+  const routes: Routes = [
+    {
+      path: 'home',
+      loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    },
+    {
+      path: '',
+      redirectTo: 'home',
+      pathMatch: 'full'
+    },
+    {
+      path: 'contact-view/:name',
+      loadChildren: () => import('./contact-view/contact-view.module').then( m => m.ContactViewPageModule)
+    },
+  ];
 
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+  @NgModule({
+    imports: [
+      RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    ],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
